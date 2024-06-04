a3571cc79a27196af30dfb9212ea5efa7cf5e809
123AbhiRaghav <abhiraghavd@gmail.com>
2024-06-04 08:37:18 +0530

    update

---
+++ index.js
@@ -0,0 +1,34 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import dbConnection from "./utils/index1.js";
+import { errorHandler,routeNotFound } from "./middleware/error.js";
+import router from './routes/route.js'
+
+const routes = "";
+
+dotenv.config();
+
+dbConnection();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors({
+   origin: ["http://localhost:3000", "http://localhost:3001"],
+   methods: ["GET", "POST", "PUT", "DELETE"],
+   credentials: true,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(morgan("dev"))
+app.use("/api", router)
+app.use(routeNotFound)
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
