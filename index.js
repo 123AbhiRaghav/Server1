5a83b51ea577b815d70e24cf4d08b2bd37b37c2e
123AbhiRaghav <abhiraghavd@gmail.com>
2024-06-06 15:07:41 +0530

    modified

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-   origin: ["http://localhost:3000", "http://localhost:3001"],
+   origin: ["http://localhost:3000", "http://localhost:3001", "https://client1-theta.vercel.app/log-in"],
    methods: ["GET", "POST", "PUT", "DELETE"],
    credentials: true,
 }));
